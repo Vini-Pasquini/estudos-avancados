2d27ebb3c21178f420f9d63cb65ffe2777b89070
Vini Pasquini <64695549+Vini-Pasquini@users.noreply.github.com>
2025-03-21 22:17:27 -0300

    audio manager - sfx

--- Assets/Code/Managers/AudioManager.cs
+++ Assets/Code/Managers/AudioManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
 
@@ -9,8 +10,12 @@ public class AudioManager
     [SerializeField] private AudioMixer _audioMixer = null;
     [SerializeField] private AudioSource _musicSource = null;
     [SerializeField] private AudioSource _sfxSource = null;
-    
+
+    [SerializeField] private int _poolSize = 10;
     [SerializeField] private List<AudioSource> _sfxSourcePool = new List<AudioSource>();
+    private GameObject _audioSourceInstance;
+
+    public delegate void AudioStop();
 
     /*
      * MUSIC
@@ -38,7 +43,6 @@ public class AudioManager
     {
         if (shouldPause) this._musicSource.Pause();
         else this._musicSource.UnPause();
-
     }
 
     public bool IsPlayingMusic()
@@ -80,5 +84,109 @@ public class AudioManager
         }
 
         this._musicSource.volume = targetVolume;
+    }
+
+    /*
+     * SFX
+     */
+
+    public void InitializeAudioPool()
+    {
+        this._audioSourceInstance = new GameObject("AudioSourceInstances");
+        this._audioSourceInstance.transform.SetParent(GameManager.Instance.transform);
+
+        for (int i = 0; i < this._poolSize; i++)
+        {
+            AudioSource newAudioSource = this._audioSourceInstance.AddComponent<AudioSource>();
+            newAudioSource.outputAudioMixerGroup = this._audioMixer.FindMatchingGroups("SFX")[0];
+            newAudioSource.playOnAwake = false;
+            this._sfxSourcePool.Add(newAudioSource);
+        }
+    }
+
+    public void PlaySFX(AudioClip sfx, float volume = 1f, float pitch = 1f)
+    {
+        this.InternalPlaySFX(sfx, volume, pitch, false);
+    }
+
+    public void PlaySFX(AudioCue sfx)
+    {
+        this.InternalPlaySFX(sfx.GetSample(), sfx.GetVolume(), sfx.GetPitch(), false);
+    }
+
+    public AudioStop PlaySFXInLoop(AudioClip sfx, float volume = 1f, float pitch = 1f)
+    {
+        return this.InternalPlaySFX(sfx, volume, pitch, true);
+    }
+
+    public AudioStop PlaySFXInLoop(AudioCue sfx)
+    {
+        return this.InternalPlaySFX(sfx.GetSample(), sfx.GetVolume(), sfx.GetPitch(), true);
+    }
+
+    public void PauseSFX(bool shouldPause)
+    {
+        if (shouldPause)
+        {
+            this._sfxSource.Pause();
+            for (int i = 0; i < _sfxSourcePool.Count; i++) { this._sfxSourcePool[i].Pause(); }
+            return;
+        }
+
+        this._sfxSource.UnPause();
+        for (int i = 0; i < _sfxSourcePool.Count; i++) { this._sfxSourcePool[i].UnPause(); }
+    }
+
+    public void StopSFX()
+    {
+        this._sfxSource.Stop();
+        for (int i = 0; i < _sfxSourcePool.Count; i++) { this._sfxSourcePool[i].Stop(); }
+    }
+
+    private AudioStop InternalPlaySFX(AudioClip sfx, float volume, float pitch, bool hasLoop = false)
+    {
+        int index = -1;
+
+        if (this._sfxSource.isPlaying)
+        {
+            for(int i = 0; i < this._sfxSourcePool.Count; i++)
+            {
+                if (this._sfxSourcePool[i].isPlaying) continue;
+                index = i;
+                break;
+            }
+            if (index == -1)
+            {
+                index = CreateAudioSource();
+            }
+        }
+
+        AudioSource audioSource = index == -1 ? _sfxSource : _sfxSourcePool[index];
+
+        audioSource.pitch = pitch;
+        audioSource.volume = volume;
+        audioSource.loop = hasLoop;
+
+        if (hasLoop)
+        {
+            audioSource.clip = sfx;
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.PlayOneShot(sfx);
+        }
+
+        AudioStop result = () => { audioSource.Stop(); };
+        return result;
+    }
+
+    private int CreateAudioSource()
+    {
+        AudioSource newAudioSource = this._audioSourceInstance.AddComponent<AudioSource>();
+        newAudioSource.outputAudioMixerGroup = this._audioMixer.FindMatchingGroups("SFX")[0];
+        newAudioSource.playOnAwake = false;
+        this._sfxSourcePool.Add(newAudioSource);
+        return this._sfxSourcePool.Count - 1;
     }
 }
--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -3,4 +3,15 @@ using UnityEngine;
 public class GameManager : IPersistentSingleton<GameManager>
 {
     public AudioManager audioManager = new AudioManager();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        this.audioManager.InitializeAudioPool();
+
+        // exemplo de utilizacao da lambda
+        //AudioManager.AudioStop audioStop = audioManager.PlaySFXInLoop(null);
+        //audioStop();
+    }
 }
